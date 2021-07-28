b57f3dc81d4127138e2b929df5ecadd23a5f3554
lilran14 <radans.works@gmail.com>
2021-07-28 07:49:23 +0700

    feat : add home screen as home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_get_device_id/bloc/device_bloc.dart';
 import 'package:flutter_get_device_id/screens/home_screen.dart';
 
 void main() {
@@ -9,12 +11,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider<DeviceBloc>(
+      create: (context) => DeviceBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Get Devive Id',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
